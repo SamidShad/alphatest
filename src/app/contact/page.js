09f8d07821d4448fc431e59948a9553b410244c8
SamidShad <samidshad@gmail.com>
2024-04-18 19:19:07 +0600

    Contact Page Added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -14,7 +14,44 @@ function Contact() {
             </p>
           </div>
         </div>
+        <div className={styles.contact_page_body_area}>
+          <div className={styles.contact_page_body_contianer}>
+            <div className={styles.contact_card}>
+              <div className={styles.contact_card_sides}>
+                <div className={styles.contact_card_side_1}>
+                  <input type="text" name="username" placeholder="Name" />
+                  <input type="text" name="useremail" placeholder="Email" />
+                  <span>Select your package :</span>
+                  <div className={styles.package_area}>
+                    <div>
+                      <input type="radio" name="basic" id="basic" checked />
+                      <label htmlFor="basic">Basic</label>
+                    </div>
+                    <div>
+                      <input type="radio" name="basic" id="pro" checked />
+                      <label htmlFor="pro">Pro</label>
+                    </div>
+                    <div>
+                      <input type="radio" name="basic" id="epic" checked />
+                      <label htmlFor="epic">Epic</label>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.contact_card_side_2}>
+                  <textarea
+                    name="usermessage"
+                    placeholder="Message"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </div>
+              </div>
+              <button className={`btn ${styles.contact_card_btn}`}>Send</button>
+            </div>
+          </div>
+        </div>
       </section>
+      <Footer />
     </>
   );
 }
