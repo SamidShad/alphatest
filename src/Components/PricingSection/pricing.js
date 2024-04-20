18f56cf8fc65128985d705e486490d7a18bc2397
SamidShad <samidshad@gmail.com>
2024-04-20 21:50:26 +0600

    bug

--- src/Components/PricingSection/pricing.js
+++ src/Components/PricingSection/pricing.js
@@ -94,7 +94,7 @@ function Pricing() {
                   <li>5GB Storage Space</li>
                 </ul>
               </div>
-              <Link href="/contact">
+              <Link href="/">
                 <button className={`btn ${styles.pricing_card_btn}`}>
                   Let's Start
                 </button>
